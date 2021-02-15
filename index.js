22105f3c31e96366881896800fa55025c0a346b6
Mathieu Quisefit <quisefit@wcentric.com>
2021-02-15 17:28:54 +0100

    Using @mathquis/node-pre-gyp-github Using @mapbox/node-pre-gyp

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var path = require('path');
-var binary = require('node-pre-gyp');
+var binary = require('@mapbox/node-pre-gyp');
 
 var binaryPath = binary.find(path.resolve(path.join(__dirname, './package.json')));
 var CRFSuite = require(binaryPath);
